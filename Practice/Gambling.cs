b5788fa9a8d673daffaab9f1da39a9486bc6d537
vidyasagar gaikwad <vidyasagargaikwad7128@gmail.com>
2021-10-06 09:55:12 +0530

    Added: UC1 Gambler Bet

---
+++ Practice/Gambling.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice
+{
+    class Gambling
+    {
+        int total = 100;
+        int bet = 1;
+        
+        public void Machine()
+        {
+            while (total!=0)
+            {
+                int random = new Random().Next(2);
+                Console.WriteLine(random);
+                total--;
+                if(total == 0)
+                {
+                    Console.WriteLine("No Money");
+                }
+            }
+        }
+    }
+}
---
+++ Practice/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+            Gambling gambling = new Gambling();
+            gambling.Machine();
+        }
+    }
+}
