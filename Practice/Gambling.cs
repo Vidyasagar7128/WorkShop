90230225afa94120665cc41c5e2570b6c7b82d7a
vidyasagar gaikwad <vidyasagargaikwad7128@gmail.com>
2021-10-06 10:42:48 +0530

    Added: Win or Loss Bet

--- Practice/Gambling.cs
+++ Practice/Gambling.cs
@@ -8,5 +8,21 @@ namespace Practice
     {
         int total = 100;
         int bet = 1;
+        
+        public void Machine()
+        {
+            while (total!=0)
+            {
+                int random = new Random().Next(2);
+                if (random == 0)
+                    total = total + 1;
+                else
+                    total = total - 1;
+                if (total == 0)
+                {
+                    Console.WriteLine("No Money");
+                }
+            }
+        }
     }
 }
