f9db07e4765d85ff583d2edc6f962dd05a46e79c
vidyasagar gaikwad <vidyasagargaikwad7128@gmail.com>
2021-10-06 10:31:42 +0530

    Added: Gambler Stake Init

--- Practice/Gambling.cs
+++ Practice/Gambling.cs
@@ -8,19 +8,5 @@ namespace Practice
     {
         int total = 100;
         int bet = 1;
-        
-        public void Machine()
-        {
-            while (total!=0)
-            {
-                int random = new Random().Next(2);
-                Console.WriteLine(random);
-                total--;
-                if(total == 0)
-                {
-                    Console.WriteLine("No Money");
-                }
-            }
-        }
     }
 }
